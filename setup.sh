8f43c29190ef73bb432da31f03677d2f6923a09f
Nick Fox-Gieg <nick@fox-gieg.com>
2026-08-10 23:22:36 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#sudo apt-get update
+#sudo apt-get install -y xvfb
+
+DIR=$PWD
+
+cd ../../../addons
+#git clone https://github.com/n1ckfg/ofxCv
+#git clone https://github.com/n1ckfg/ofxCvPiCam
+git clone https://github.com/n1ckfg/ofxNaplps
+
+cd $DIR
